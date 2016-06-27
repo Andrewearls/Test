60757a74ea43619aa65ad912086f8fd74cac741f
Andrew Earls <andrewearls@rocketmail.com>
2016-06-26 18:29:56 -0700

    added intro to python

---
+++ teach.py
@@ -0,0 +1,68 @@
+#This is a comment
+
+#shell commands
+num = raw_input('Enter a number: ') # allows you to ask a question and get user
+                                    #input from the shell
+print "your number = " + num        #Allow you to print to shell
+
+#these are variable
+intager = 0 #whole number
+real = 1.5 #a number with a decimal
+char = 'd' #a single character
+string = "hello" #an array of characters
+boolian = True # or False
+
+#Variable are boxes that can be named almost anything and that can contain
+#almost any type of data
+
+Tuple = (1,"hello") #A variable that holds multiple pices of data
+                    #Declared with parenthises and seperated by at
+                    #least one comma
+
+array = [1, Tuple]  #A variable that can hold multiple pices of data
+                    #Declared with brackets and speperated by commas
+
+#statments
+#if staments as a question and return a true or false
+#if stament is true compleate task.
+#if false do else
+
+if num == "5":
+    string = "2 + 3 = " + num
+
+#if the statment is false then pass the if statment
+#elif catches the process and askes an new question only
+#if the previous if statment is false
+elif string == "hello":
+    print string
+
+#else actches all other possiblilities.
+#if all previous statments are false else will compleate any folloing task.
+else:
+    print "string does not equal win or hello"
+
+#operators
+#operators are mathmatical symbals that allow the program to compare multiple
+#operans (or variables) against each other
+
+opperands = r"!, =, !=, ==, +, -, *, /, %"
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
